2c308e52596ed3643c2150f12d96cb4d8c3c821c
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-08 14:01:50 -0500

    Giant collider removed

---
+++ AlphaDemo/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+    public float maxTime;
+
+    float gameTimer;
+    bool gameEnded;
+    
+    void Update()
+    {
+        updateTimer();
+        if (checkPlayerTimeEnded())
+        {
+
+        }
+    }
+
+    void updateTimer()
+    {
+        gameTimer = Mathf.MoveTowards(gameTimer, 0, Time.deltaTime);
+    }
+
+    bool checkPlayerTimeEnded()
+    {
+        return false;
+    }
+
+    public float getGameTimer()
+    {
+        return gameTimer;
+    }
+
+}
