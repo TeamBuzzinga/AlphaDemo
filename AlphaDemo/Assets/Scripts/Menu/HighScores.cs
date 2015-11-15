f29cce4d3042291a0c6397f25547b1ba93a7390f
Siddharth <siddharthshah52@gmail.com>
2015-11-15 17:03:28 -0500

    Basic Menu
    To Do:
    High Score implementation contains an incomplete script. Not sure how to
    implement.
    Credits screen needs that text animation.
    Press different buttons to test their implementation for now

---
+++ AlphaDemo/Assets/Scripts/Menu/HighScores.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Linq;
+
+public class HighScores : MonoBehaviour {
+
+	public string FileName;
+	private TextAsset textAsset;
+	private StreamWriter streamWriter;
+	private string lastScore;
+	private string[] tempArr;
+	private int score = 50;
+	private string playerName = "Sid";
+	private int i;
+
+	public void appendScore() {
+		textAsset = Resources.Load (FileName + ".txt") as TextAsset;
+		lastScore = File.ReadAllLines ("Resources/" + FileName + ".txt").Last ();
+		tempArr = lastScore.Split ((char[])null);
+		lastScore = tempArr[tempArr.Length];
+		if (int.TryParse (lastScore, out i)) {
+			if (score > i) {
+				streamWriter = new StreamWriter ("Resources/" + FileName + ".txt");
+				streamWriter.WriteLine (playerName + " " + lastScore + "\n");
+				tempArr = File.ReadAllLines("Resources/" + FileName + ".txt");
+			}
+		}
+	}
+}
