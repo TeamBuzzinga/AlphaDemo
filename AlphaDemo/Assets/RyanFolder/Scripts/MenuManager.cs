4758b2092b5860ca6c4d7a6b919444e20a3054a6
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-16 00:01:36 -0500

    Updates to RyanMainMenu

---
+++ AlphaDemo/Assets/RyanFolder/Scripts/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+    Animator menuAnim;
+
+    void Start()
+    {
+        menuAnim = GetComponent<Animator>();
+    }
+
+    void Upate()
+    {
+
+    }
+
+    public void optionsPressed()
+    {
+
+    }
+
+    public void playButtonPressed()
+    {
+
+    }
+
+    public void creeditsButtonPresed()
+    {
+
+    }
+}
