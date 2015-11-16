1b8b792b4a718549adc706679845831e811dfe52
dingfs <dingfengshao@gmail.com>
2015-11-16 14:34:01 -0500

    Create a dizzy particle effect

---
+++ AlphaDemo/Assets/Dingfengprefab/rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+	// Use this for initialization
+    Vector3 _rotate;
+	void Start () {
+        _rotate = new Vector3(0,90,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(_rotate * Time.deltaTime);
+	}
+}
