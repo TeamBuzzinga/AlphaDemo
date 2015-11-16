96f4f6543274dac1b914f9149da03a897fbb9fb8
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-16 14:45:08 -0500

    Credits Screen implemented. Needs text!

--- AlphaDemo/Assets/RyanFolder/Scripts/MenuManager.cs
+++ AlphaDemo/Assets/RyanFolder/Scripts/MenuManager.cs
@@ -23,12 +23,18 @@ public class MenuManager : MonoBehaviour {
 
     public void playButtonPressed()
     {
-
+        Application.LoadLevel("Office v1.3");
     }
 
     public void creditsButtonPresed()
     {
         cameraAnimator.SetTrigger("Credits");
         menuAnim.SetTrigger("Credits");
+    }
+
+    public void toIntroScreen()
+    {
+        cameraAnimator.SetTrigger("Intro");
+        menuAnim.SetTrigger("Intro");
     }
 }
